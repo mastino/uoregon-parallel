0e637ff1ae4ef50d593fa8939aff8f60c0fa2ae1
tomc <tomc@cs.uoregon.edu>
2015-04-10 06:35:58 -0700

    lab 2 & 3

---
+++ labs/lab2/openmpNotQuiteRight.c
@@ -0,0 +1,26 @@
+#define ARRAYSIZE 100
+
+void init(int* a, int size){
+  int i=0;
+  for(i=0;i<size;i++){
+    a[i]=i;
+  }
+}
+
+int main() {
+   int i=0;
+   int* a=(int*)malloc(sizeof(int)*ARRAYSIZE);
+   init(b,ARRAYSIZE);
+   omp_set_num_threads(4);
+   //intended behavior   : a[i] gets the sum of its value and its next (cyclic) neighbor's initial value.
+   //observed behavior   : inconsistent quasi cumulative.
+   //Luckily we have Computer Scientists here to fix this for us.
+   #pragma omp parallel for
+   for(i=0;i<=ARRAYSIZE;i++){
+      a[i]=a[i]+a[((i-1)%ARRAYSIZE)];
+   }
+   
+   for(i=0;i<ARRAYSIZE;i++){
+      printf("Index: %d, Value: %d\n",i,a[i]);
+   }
+}
