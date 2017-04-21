81c6d48f76f870467fbe89e94a1398c7c385949e
Brian Gravelle <bgravelle5@gmail.com>
2017-04-21 12:51:47 -0700

    lab1 complete

--- labs/lab1/helloWorld.c
+++ labs/lab1/helloWorld.c
@@ -1,13 +1,14 @@
-#include <omp.h>
+  #include <omp.h>
 #include <time.h>
-#define NUM_ITERS 10
+#define NUM_ITERS 20
 
 int main() {
   int i;
   #pragma omp parallel for
   for(i=0;i<NUM_ITERS;i++) {
     int thread_num=omp_get_thread_num();
-    printf("Alien #%d says hello, they're  #%d in line.\n",thread_num,i);
+    int num_threads=omp_get_num_threads();
+    printf("thread %d of %d\n",thread_num,num_threads);
     sleep(1);
   }
 }
